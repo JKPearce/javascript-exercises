f7c0da6ef68166c55220d1409fdf579c40ffed3b
Jaydon <pearcejaydon@gmail.com>
2022-07-24 15:02:57 +1000

    Completed exercises for javascript fundamentals part 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    // let splitString = s.split("");
+    // let reverseArray = splitString.reverse();
+    // let joinArray = reverseArray.join("");
+    // return joinArray;
+    return s.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let array = args[0];
+    let modifiedArray = [];
 
+    array.forEach((item) =>{
+    if(!args.includes(item)) {
+        modifiedArray.push(item);
+     }
+    });
+
+    return modifiedArray;
 };
 
 // Do not edit below this line
